1fb7dd3c690a6f6f30a9860c42eaae5d825cea60
nazar.rybchuk <nazar.rybchuk@atlas-iac.com>
2023-05-10 22:50:37 +0300

    add track function

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,17 @@
 var str = document.getElementById("str");
 var des_str = document.getElementById("des_str");
 var all = document.getElementById("all");
+var last_text = document.getElementById("last_text");
+
+function track (selector) {
+  var elements =  selector.querySelector("#tinymce").childNodes;
+  var lastText = [];
+  for(let i  = 0; i<elements.length; i++){
+    lastText.push(elements[i].outerHTML);
+  }
+  console.log(lastText.join(''));
+  localStorage.setItem('test', lastText.join(''));
+}
 
 str.addEventListener("click", async(e) => {
   chrome.tabs.update({}, async (tab) => {
@@ -13,6 +24,20 @@ str.addEventListener("click", async(e) => {
 
         if (iframeDocument.querySelector("#tinymce") !== null) {
           iframeDocument.querySelector("#tinymce").innerHTML = text_str
+
+          // start tracked last text
+          iframeDocument.oninput = function() {
+            var elements =  iframeDocument.querySelector("#tinymce").childNodes;
+            var lastText = [];
+            for(let i  = 0; i<elements.length; i++){
+              lastText.push(elements[i].outerHTML);
+            }
+            console.log(lastText.join(''));
+            localStorage.setItem('test', lastText.join(''));
+          }
+          // finish
+
+          
         } else {
           var lastFocusedElement = document.activeElement;
           var lastIframeDocument = lastFocusedElement.contentDocument || iframe.contentWindow.document;
@@ -35,6 +60,20 @@ des_str.addEventListener("click", function () {
 
         if (iframeDocument.querySelector("#tinymce") !== null) {
           iframeDocument.querySelector("#tinymce").innerHTML = text_des_str
+
+                    // start tracked last text
+                    iframeDocument.oninput = function() {
+                      var elements =  iframeDocument.querySelector("#tinymce").childNodes;
+                      var lastText = [];
+                      for(let i  = 0; i<elements.length; i++){
+                        lastText.push(elements[i].outerHTML);
+                      }
+                      console.log(lastText.join(''));
+                      localStorage.setItem('test', lastText.join(''));
+                    }
+                    // finish
+
+
         } else {
           var lastFocusedElement = document.activeElement;
           var lastIframeDocument = lastFocusedElement.contentDocument || iframe.contentWindow.document;
@@ -57,6 +96,20 @@ all.addEventListener("click", function () {
 
         if (iframeDocument.querySelector("#tinymce") !== null) {
           iframeDocument.querySelector("#tinymce").innerHTML = text_all;
+
+                    // start tracked last text
+                    iframeDocument.oninput = function() {
+                      var elements =  iframeDocument.querySelector("#tinymce").childNodes;
+                      var lastText = [];
+                      for(let i  = 0; i<elements.length; i++){
+                        lastText.push(elements[i].outerHTML);
+                      }
+                      console.log(lastText.join(''));
+                      localStorage.setItem('test', lastText.join(''));
+                    }
+                    // finish
+
+                    
         } else {
           var lastFocusedElement = document.activeElement;
           var lastIframeDocument = lastFocusedElement.contentDocument || iframe.contentWindow.document;
@@ -67,3 +120,61 @@ all.addEventListener("click", function () {
     });
   });
 });
+
+last_text.addEventListener("click", function () {
+  chrome.tabs.update({}, async (tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+
+        var iframe = document.querySelector('iframe');
+        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+        if (iframeDocument.querySelector("#tinymce") !== null) {
+          iframeDocument.querySelector("#tinymce").innerHTML = localStorage.getItem('test')
+        } else {
+          var lastFocusedElement = document.activeElement;
+          var lastIframeDocument = lastFocusedElement.contentDocument || iframe.contentWindow.document;
+          lastIframeDocument.querySelector("#tinymce").innerHTML = localStorage.getItem('test')
+        }
+
+      }
+    });
+  });
+});
+
+
+// chrome.tabs.update({}, async (tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: () => {
+
+//       var iframe = document.querySelector('iframe');
+//       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+//         iframeDocument.oninput = function() {
+//           var elements =  iframeDocument.querySelector("#tinymce").childNodes;
+//           var lastText = [];
+//           for(let i  = 0; i<elements.length; i++){
+//             lastText.push(elements[i].outerHTML);
+//           }
+//           console.log(lastText.join(''));
+
+//           // var lastText = elements.forEach(element => element.outerHTML);
+
+//           localStorage.setItem('test', lastText.join(''));
+
+//           // chrome.storage.local.set({ 'cusmon':  helo}).then(() => {
+//           //   console.log("Value is set to " + helo);
+//           // });
+
+//           // chrome.storage.local.get(['cusmon']).then((result) => {
+//           //   console.log("Value currently is " + result.key);
+//           // });
+
+//         };
+
+
+//     }
+//   });
+// });
